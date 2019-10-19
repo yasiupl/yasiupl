1a2cc44c7edfc77c48c1f373b94de3c89aa12b93
Marcin Jasiukowicz <contact@yasiu.pl>
2019-10-19 21:43:51 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function () {
+                console.log('rocket.watch serviceworker install successful');
+            })
+
+            .catch(function (err) {
+                console.log('rocket.watch serviceworker install failed: ', err);
+            });
+    });
+}
+
+
 fetch(".netlify/functions/lastfm?t=" + Date.now(), {
     headers: {
       "Accept": "application/json"
